5bfab4d475379c5704c33a6f8508e605869dcb0f
Volodymyr Havryliuk <chotkiypaca1@gmail.com>
2024-06-18 16:34:48 +0300

    init commit

---
+++ custom-validation-implementation/src/main/java/com/ukma/validator/StudentCustomValidator.java
@@ -0,0 +1,34 @@
+package com.ukma.validator;
+
+import java.util.List;
+import com.google.common.collect.Lists;
+import com.ukma.entity.Student;
+import com.ukma.string.utils.StringValidationUtils;
+
+public class StudentCustomValidator implements CustomValidator {
+
+    @Override
+    public List<String> validate(Object object) {
+        List<String> errors = Lists.newArrayList();
+        if (object == null) {
+            errors.add("student is null!");
+            return errors;
+        }
+
+        Student student = (Student) object;
+        if (!StringValidationUtils.isEmail(student.getEmail())) {
+            errors.add("email format is not correct!");
+        }
+        if (!StringValidationUtils.isBlank(student.getName())) {
+            errors.add("student's name is blank!");
+        }
+        if (!StringValidationUtils.isUkrainianPhone(student.getPhone())) {
+            errors.add("phone format is not correct!");
+        }
+        if (!StringValidationUtils.isAllUpperCase(student.getFaculty())) {
+            errors.add("faculty name's characters should all be upper case!");
+        }
+
+        return errors;
+    }
+}
